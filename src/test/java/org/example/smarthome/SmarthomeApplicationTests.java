2b43c30f9efb8bd66d97de086d0f0bd4d3ade845
combrige <combrige47@gmail.com>
2025-08-27 17:24:17 +0800

    Initial commit: 初始化项目

---
+++ src/test/java/org/example/smarthome/SmarthomeApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.smarthome;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmarthomeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
